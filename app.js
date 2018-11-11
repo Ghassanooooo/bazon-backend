6c9e282f94b8cd2001df874a8b5321b755c8514c
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-11-11 15:44:15 +0100

    fixed socket problem

--- app.js
+++ app.js
@@ -1,42 +1,30 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const http = require("http");
+const socketIo = require("socket.io");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb://ghassan:ghassan89@ds159293.mlab.com:59293/bazon-dev",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("mongodb connected"))
+  .catch(err => console.log(err));
 
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const server = http.createServer(app);
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(logger("dev"));
-app.use(express.json());
+const io = socketIo(server);
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+io.on("connection", function(socket) {
+  console.log("the client connected");
+  io.emit("posts", { email: "ghassan@yahoo.com" });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+server.listen(PORT, () => {
+  console.log(`server run with port: ${PORT}`);
 });
-
-module.exports = app;
